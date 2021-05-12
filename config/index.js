8550459892b0eca18d11ebd1b4bbf24fbe113149
Luis Vazquez <luvazpa@gmail.com>
2021-05-12 13:05:02 -0500

    Environment variable added

--- config/index.js
+++ config/index.js
@@ -5,7 +5,8 @@ const config = {
   port: process.env.PORT || 3001,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
-  dbName: process.env.DB_NAME
+  dbName: process.env.DB_NAME,
+  dbHost: process.env.DB_HOST
 }
 
 module.exports = config
